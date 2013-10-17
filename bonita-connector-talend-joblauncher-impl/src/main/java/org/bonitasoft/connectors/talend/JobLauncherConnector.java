971f6a970da46cada88d30b2ca84dae1ba809d08
Romain Bioteau <romain.bioteau@bonitasoft.com>
2013-10-17 11:46:36 +0200

    BI-320 Improve error log

--- bonita-connector-talend-joblauncher-impl/src/main/java/org/bonitasoft/connectors/talend/JobLauncherConnector.java
+++ bonita-connector-talend-joblauncher-impl/src/main/java/org/bonitasoft/connectors/talend/JobLauncherConnector.java
@@ -35,133 +35,138 @@ import routines.system.api.TalendJob;
  */
 public class JobLauncherConnector extends AbstractConnector {
 
-    private static final String RUN_JOB_METHOD = "runJob";
+	private static final String RUN_JOB_METHOD = "runJob";
 
-    private static final String CONTEXT_PARAM_PREFIX = "--context_param:";
+	private static final String CONTEXT_PARAM_PREFIX = "--context_param:";
 
-    private static final String CONTEXT_PARAM_SEPARATOR = "=";
+	private static final String CONTEXT_PARAM_SEPARATOR = "=";
 
-    private static final String PROJECT_NAME = "projectName";
+	private static final String PROJECT_NAME = "projectName";
 
-    private static final String JOB_NAME = "jobName";
+	private static final String JOB_NAME = "jobName";
 
-    private static final String JOB_VERSION = "jobVersion";
+	private static final String JOB_VERSION = "jobVersion";
 
-    private static final String JOB_PARAMETERS = "jobParameters";
+	private static final String JOB_PARAMETERS = "jobParameters";
 
-    private static final String PRINT_OUTPUT = "printOutput";
+	private static final String PRINT_OUTPUT = "printOutput";
 
-    private static final String BUFFER_OUTPUT = "bufferOutput";
+	private static final String BUFFER_OUTPUT = "bufferOutput";
 
-    // IN
-    private String projectName;
+	// IN
+	private String projectName;
 
-    private String jobName;
+	private String jobName;
 
-    private String jobVersion;
+	private String jobVersion;
 
-    private Map<String, String> jobParameters;
+	private Map<String, String> jobParameters;
 
-    private boolean printOutput;
+	private boolean printOutput;
 
-    // OUT
-    private java.lang.String[][] bufferOutput;
+	// OUT
+	private java.lang.String[][] bufferOutput;
 
-    public java.lang.String[][] getBufferOutput() {
-        return (String[][]) getOutputParameters().get(BUFFER_OUTPUT);
-    }
+	public java.lang.String[][] getBufferOutput() {
+		return (String[][]) getOutputParameters().get(BUFFER_OUTPUT);
+	}
 
-    @Override
-    protected void executeBusinessLogic() throws ConnectorException {
-        final String jobClassName = projectName.toLowerCase() + "." + jobName.toLowerCase() + "_" + jobVersion.replace('.', '_') + "." + jobName;
-        try {
-            final Class<?> clazz = Class.forName(jobClassName, true, Thread.currentThread().getContextClassLoader());
+	@Override
+	protected void executeBusinessLogic() throws ConnectorException {
+		final String jobClassName = projectName.toLowerCase() + "." + jobName.toLowerCase() + "_" + jobVersion.replace('.', '_') + "." + jobName;
+		try {
+			Class<?> clazz = null;
+			try{
+				clazz = Class.forName(jobClassName, true, Thread.currentThread().getContextClassLoader());
+			}catch(ClassNotFoundException cnfe){
+				throw new ConnectorException("The TalendJob class "+jobClassName+" has not been found in the process classpath.");
+			}
 
-            // final Method runJobMethod = clazz.getMethod(RUN_JOB_METHOD, new Class[] { String[].class });
-            // final Object jobInstance = clazz.newInstance();
-            final TalendJob jobInstance = (TalendJob) clazz.newInstance();
+			// final Method runJobMethod = clazz.getMethod(RUN_JOB_METHOD, new Class[] { String[].class });
+			// final Object jobInstance = clazz.newInstance();
+			final TalendJob jobInstance = (TalendJob) clazz.newInstance();
 
-            Collection<String> jobParams = new ArrayList<String>();
-            if (jobParameters != null) {
-                for (Map.Entry<String, String> parameter : jobParameters.entrySet()) {
-                    jobParams.add(CONTEXT_PARAM_PREFIX + parameter.getKey() + CONTEXT_PARAM_SEPARATOR + parameter.getValue());
-                }
-            }
+			Collection<String> jobParams = new ArrayList<String>();
+			if (jobParameters != null) {
+				for (Map.Entry<String, String> parameter : jobParameters.entrySet()) {
+					jobParams.add(CONTEXT_PARAM_PREFIX + parameter.getKey() + CONTEXT_PARAM_SEPARATOR + parameter.getValue());
+				}
+			}
 
-            // bufferOutput = (java.lang.String[][]) runJobMethod.invoke(jobInstance, new Object[] { jobParams.toArray(new String[] {}) });
-            bufferOutput = jobInstance.runJob(jobParams.toArray(new String[] {}));
+			// bufferOutput = (java.lang.String[][]) runJobMethod.invoke(jobInstance, new Object[] { jobParams.toArray(new String[] {}) });
+			bufferOutput = jobInstance.runJob(jobParams.toArray(new String[] {}));
 
-            setOutputParameter(BUFFER_OUTPUT, bufferOutput);
+			setOutputParameter(BUFFER_OUTPUT, bufferOutput);
 
-            if (printOutput) {
-                printBufferOutput();
-            }
-        } catch (Exception e) {
-            throw new ConnectorException(e);
-        }
-        // use case : use talend to get all products list: first column in french, second in english. TO be mapped to a combo box for example
-        // add an input to say where output must be stored
-    }
+			if (printOutput) {
+				printBufferOutput();
+			}
+		} catch (Exception e) {
+			throw new ConnectorException(e);
+		}
+		// use case : use talend to get all products list: first column in french, second in english. TO be mapped to a combo box for example
+		// add an input to say where output must be stored
+	}
 
-    @Override
-    public void validateInputParameters() throws ConnectorValidationException {
-        projectName = (String) getInputParameter(PROJECT_NAME);
-        jobName = (String) getInputParameter(JOB_NAME);
-        jobVersion = (String) getInputParameter(JOB_VERSION);
-        jobParameters = setJobParameters((List<List<Object>>) getInputParameter(JOB_PARAMETERS));
-        printOutput = (Boolean) getInputParameter(PRINT_OUTPUT);
+	@Override
+	public void validateInputParameters() throws ConnectorValidationException {
+		projectName = (String) getInputParameter(PROJECT_NAME);
+		jobName = (String) getInputParameter(JOB_NAME);
+		jobVersion = (String) getInputParameter(JOB_VERSION);
+		jobParameters = setJobParameters((List<List<Object>>) getInputParameter(JOB_PARAMETERS));
+		printOutput = (Boolean) getInputParameter(PRINT_OUTPUT);
 
-        List<String> errors = new ArrayList<String>();
+		List<String> errors = new ArrayList<String>();
 
-        if (projectName == null || projectName.trim().length() == 0) {
-            errors.add("projectName cannot be empty");
-        }
-        if (jobName == null || jobName.trim().length() == 0) {
-            errors.add("jobName cannot be empty");
-        }
-        if (jobVersion == null || jobVersion.trim().length() == 0) {
-            errors.add("jobVersion cannot be empty");
-        }
+		if (projectName == null || projectName.trim().length() == 0) {
+			errors.add("projectName cannot be empty");
+		}
+		if (jobName == null || jobName.trim().length() == 0) {
+			errors.add("jobName cannot be empty");
+		}
+		if (jobVersion == null || jobVersion.trim().length() == 0) {
+			errors.add("jobVersion cannot be empty");
+		}
 
-        if (!errors.isEmpty()) {
-            throw new ConnectorValidationException(this, errors);
-        }
-    }
+		if (!errors.isEmpty()) {
+			throw new ConnectorValidationException(this, errors);
+		}
+	}
 
-    private void printBufferOutput() {
-        System.out.println("Buffer output for job: " + jobName + " " + jobVersion + " (project=" + projectName + "):");
-        int line = 0;
-        for (String[] columns : getBufferOutput()) {
-            System.out.print("Line " + line + ": ");
-            int column = 0;
-            for (String s : columns) {
-                System.out.print(s);
-                if (column < (columns.length - 1)) {
-                    System.out.print(", ");
-                }
-                column++;
-            }
-            System.out.println();
-            line++;
-        }
-    }
+	private void printBufferOutput() {
+		System.out.println("Buffer output for job: " + jobName + " " + jobVersion + " (project=" + projectName + "):");
+		int line = 0;
+		for (String[] columns : getBufferOutput()) {
+			System.out.print("Line " + line + ": ");
+			int column = 0;
+			for (String s : columns) {
+				System.out.print(s);
+				if (column < (columns.length - 1)) {
+					System.out.print(", ");
+				}
+				column++;
+			}
+			System.out.println();
+			line++;
+		}
+	}
 
-    private Map<String, String> setJobParameters(final List<List<Object>> jobParametersList) {
-        Map<String, String> jobParametersMap = new HashMap<String, String>();
-        if (jobParametersList != null) {
-            for (List<Object> rows : jobParametersList) {
-                if (rows.size() == 2) {
-                    Object keyContent = rows.get(0);
-                    Object valueContent = rows.get(1);
-                    if (keyContent != null && valueContent != null) {
-                        final String key = keyContent.toString();
-                        final String value = valueContent.toString();
-                        jobParametersMap.put(key, value);
-                    }
-                }
-            }
-        }
-        return jobParametersMap;
-    }
+	private Map<String, String> setJobParameters(final List<List<Object>> jobParametersList) {
+		Map<String, String> jobParametersMap = new HashMap<String, String>();
+		if (jobParametersList != null) {
+			for (List<Object> rows : jobParametersList) {
+				if (rows.size() == 2) {
+					Object keyContent = rows.get(0);
+					Object valueContent = rows.get(1);
+					if (keyContent != null && valueContent != null) {
+						final String key = keyContent.toString();
+						final String value = valueContent.toString();
+						jobParametersMap.put(key, value);
+					}
+				}
+			}
+		}
+		return jobParametersMap;
+	}
 
 }
